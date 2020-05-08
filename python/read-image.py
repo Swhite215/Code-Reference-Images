2619c5c507c7313c017b45ff13516403c451a3a6
Spencer White <swhit210@ford.com>
2020-05-07 20:29:18 -0400

    convert to function and added execution timing

--- python/read-image.py
+++ python/read-image.py
@@ -1,9 +1,14 @@
 from PIL import Image
+import timeit
 
-im = Image.open("../simple-pixels/1x1_WHITE.png");
+def read_image():
+    im = Image.open("../simple-pixels/1x1_WHITE.jpg")
 
-# Prints Image Format, Size NxN, and Mode
-print(im.format, im.size, im.mode)
+    # Prints Image Format, Size NxN, and Mode
+    print(im.format, im.size, im.mode)
 
-# Shows an Image
-im.show()
+    # Shows an Image
+    im.show()
+
+# Execution Time = .0214
+print(timeit.timeit(read_image, number=1))
