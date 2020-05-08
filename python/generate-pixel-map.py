5f2aaf99a3c8f90c26e9a218517080390434ba1c
Spencer White <swhit210@ford.com>
2020-05-07 20:29:14 -0400

    convert to function and added execution timing

--- python/generate-pixel-map.py
+++ python/generate-pixel-map.py
@@ -1,25 +1,26 @@
 from PIL import Image
+import timeit
 
-# Get Image
-im = Image.open("../simple-pixels/3x6_CARTESIAN_TEST.png");
+def generate_pixel_map():
+    # Get Image
+    im = Image.open("../simple-pixels/3x6_CARTESIAN_TEST.png")
 
-# Convert Image to Pixel Map
-pixels = im.load()
+    # Convert Image to Pixel Map
+    pixels = im.load()
 
-# Create Pixel Coordinate Matrix Static
-count = 0
+    # Create Pixel Coordinate Matrix Static
+    count = 0
 
-pixelCoordinateMatrix = [[0 for i in range(im.size[0])] for j in range(im.size[1])]
+    pixelCoordinateMatrix = [[0 for i in range(im.size[0])] for j in range(im.size[1])]
 
-# THE BELOW REUSES A LIST - EACH CHANGE TO A CELL WILL CHANGE ALL LISTS WITH THE SAME POINTER
-#WTFpixelCoordinateMatrix = [[0] * im.size[0]] * im.size[1]
-#WTFpixelCoordinateMatrix[0][0] = 10
+    # Generate a Pixel Map and Color Map
+    for row in range(im.size[1]):
+        for column in range(im.size[0]):
+            print("Row: ", row, "Column: ", column)
+            pixelCoordinateMatrix[row][column] = ((row, column))
+            count += 1
 
-# Generate a Pixel Map and Color Map
-for row in range(im.size[1]):
-    for column in range(im.size[0]):
-        print("Row: ", row, "Column: ", column)
-        pixelCoordinateMatrix[row][column] = ((row, column))
-        count += 1
+    print(pixelCoordinateMatrix)
 
-print(pixelCoordinateMatrix)
+# Execution Time = .013
+print(timeit.timeit(generate_pixel_map, number=1))
