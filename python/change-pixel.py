3d95c065247574938ec89f96e4c060ef4c61edf0
Spencer White <swhit210@ford.com>
2020-05-07 20:29:00 -0400

    convert to function and added execution timing

--- python/change-pixel.py
+++ python/change-pixel.py
@@ -1,12 +1,16 @@
 from PIL import Image
+import timeit
 
-im = Image.open("../simple-pixels/1x1_WHITE.jpg");
+def change_pixel():
+    im = Image.open("../simple-pixels/1x1_WHITE.jpg");
 
-# Convert Image to Pixel Map
-pixels = im.load()
+    # Convert Image to Pixel Map
+    pixels = im.load()
 
-# Change A Single Pixel to Different Color
-pixels[0,0] = (0,255,0,255)
+    # Change A Single Pixel to Different Color
+    pixels[0,0] = (0,255,0,255)
 
-# Check Results
-im.show()
+    # Check Results
+    im.show()
+
+print(timeit.timeit(change_pixel, number=1))
