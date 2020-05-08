96e316deef0245a3396d7de0154aada673481614
Spencer White <swhit210@ford.com>
2020-05-07 20:29:28 -0400

    first lesson added

---
+++ python/learnings.py
@@ -0,0 +1,3 @@
+# THE BELOW REUSES A LIST - EACH CHANGE TO A CELL WILL CHANGE ALL LISTS WITH THE SAME POINTER
+#WTFpixelCoordinateMatrix = [[0] * im.size[0]] * im.size[1]
+#WTFpixelCoordinateMatrix[0][0] = 10
