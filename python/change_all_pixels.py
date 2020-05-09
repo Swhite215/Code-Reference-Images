a614c84799cac808ff85951ecd1086fe9fdb5a85
Spencer White <swhit210@ford.com>
2020-05-09 13:20:37 -0400

    adjustment to use numarry shape for dimensions

--- python/change_all_pixels.py
+++ python/change_all_pixels.py
@@ -1,4 +1,4 @@
-#generate_numpy.py
+
 from PIL import Image
 import numpy as np
 import timeit
@@ -6,22 +6,27 @@ import timeit
 def change_all_pixels():
     targetColor = [0, 0, 0, 255]
 
-    oldImagePath = "../simple-pixels/2x2_CARTESIAN_TEST.png"
-    newImagePath = "../new-images/2x2_CARTESTIAN_BLACK.png"
+    oldImagePath = "../complex-pixels/ECRIC_UNCOLORIZED.png"
+    newImagePath = "../new-images/ECRIC_BLACKED.png"
 
     im = Image.open(oldImagePath)
+
+    print(im.format, im.size, im.mode)
 
     # Convert Image to NumpyArray
     numArray = np.array(im)
 
+    print(numArray.shape[0])
+    print(numArray.shape[1])
+
     # Get Pixels
-    width = im.size[0]
-    height = im.size[1]
+    width = numArray.shape[0]
+    height = numArray.shape[1]
 
     # Change Each Pixel to Target Color
     for row in range(width):
         for column in range(height):
-            print(numArray[row, column])
+            #print(numArray[row, column])
             numArray[row, column] = [0, 0, 0, 255]
 
     # Convert NumpyArray to Image
